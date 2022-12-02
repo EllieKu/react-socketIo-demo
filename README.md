73eaa9c54fb1fbedc21f08c35d124d750228f095
Ellie <eli52583@hotmail.com>
2022-12-02 17:12:06 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-socketIo-demo
+
+***
+
+ReactJs + Socket.io
+
+this project must be used with socketio-server-demo project
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import io from "socket.io-client"
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [room, setRoom] = useState("")
+
+  const [message, setMessage] = useState("")
+  const [messageReceive, setMessageReceive] = useState("")
+
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('join_room', room)
+    }
+  }
+
+  const sendMessage = () => {
+    socket.emit('send_message', { message, room })
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceive(data.message)
+    })
+  }, [socket])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        onChange={(e) => setRoom(e.target.value)}
+        placeholder='Room'
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder='Message...'
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      <p>{messageReceive}</p>
     </div>
   )
 }
